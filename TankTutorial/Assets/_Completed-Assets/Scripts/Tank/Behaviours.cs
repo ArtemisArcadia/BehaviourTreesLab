11a952fd7cd5bfda7c0a10902e7279442f4eff4b
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2017-10-27 00:03:34 +0100

    Last commit was broken, fixed it

--- TankTutorial/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ TankTutorial/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -84,14 +84,19 @@ namespace Complete
 				new Service (0.2f, UpdatePerception,
 					new Selector (
 						new BlackboardCondition ("targetDistance",
-							Operator.IS_GREATER_OR_EQUAL, 0.1f,
+							Operator.IS_SMALLER_OR_EQUAL, 10f,
 							Stops.IMMEDIATE_RESTART,
 							new Sequence(
 								new Action(() => Move(1)),
 								new Action(() => Turn(1)),
-								new Action(() => Turn(-1),
+								new Action(() => Turn(-1)),
 								new Action(() => Fire(0))
 							)
+						),
+						new BlackboardCondition ("targetDistance",
+							Operator.IS_GREATER_OR_EQUAL, 11f,
+							Stops.IMMEDIATE_RESTART,
+							StopMoving()
 						)
 					)
 				)
@@ -115,37 +120,39 @@ namespace Complete
 						new BlackboardCondition("objectBlock", Operator.IS_EQUAL,true,
 							Stops.IMMEDIATE_RESTART,
 							new Sequence(
-								new Action(() => Move(-1)),
-								//new Action(() => Turn(2)),
-								new Action(() => StopTurning()))),
+								new Action(() => Turn(1)),
+								new Wait(0.2f),
+								StopTurning())),
 
 						new BlackboardCondition("objectBehind", Operator.IS_EQUAL,true,
 							Stops.IMMEDIATE_RESTART,
 							new Sequence(
-								new Action(() => Move(1)),
-								new Action(() => Turn(2)),
-								new Action(() => StopTurning()))),
+								new Action(() => Move(0.1f)),
+								new Wait(1f),
+								new Action(() => Turn(1)),
+								new Wait(0.1f),
+								StopTurning())),
 						
 						new BlackboardCondition ("targetOffCentre",
 							Operator.IS_SMALLER_OR_EQUAL, 0.1f,
 							Stops.IMMEDIATE_RESTART,
 
 							new Sequence (
-								new Action (() => StopTurning ()),
-								new Action (() => Fire (1)))),
+							 StopTurning (),
+								new Action (() => Fire (0.5f)))),
 
 						new BlackboardCondition ("targetDistance",
 							Operator.IS_GREATER_OR_EQUAL, 10f,
 							Stops.IMMEDIATE_RESTART,
 
 							
-								new Action (() => Move (1))),
+								new Action (() => Move (0.5f))),
 
 						new BlackboardCondition ("targetDistance",
-							Operator.IS_SMALLER_OR_EQUAL, 5f,
+							Operator.IS_SMALLER_OR_EQUAL, 3f,
 							Stops.IMMEDIATE_RESTART,
 
-							new Action (() => Move (-1))),
+							new Action (() => Move (0.5f))),
 
 
 						new BlackboardCondition ("targetOnRight",
@@ -218,9 +225,9 @@ namespace Complete
 
 
 							// Turn left away target
-							new Action (() => Turn (1)))
+							new Action (() => Turn (1))),
 						// Turn right
-						// 	new Action (() => Turn (1))
+						 	new Action (() => Turn (1))
 
 
 
@@ -237,9 +244,8 @@ namespace Complete
             Vector3 heading = localPos.normalized;
 			Vector3 Front = transform.TransformDirection (Vector3.forward); //variable that holds Vector3 forward value
 			Vector3 Behind = transform.TransformDirection (Vector3.back); //variable that holds Vector 3 back value
-			bool objectBlocking = Physics.Raycast (transform.position, Front, 1); //checks to see if something is close in front of it
+			bool objectBlocking = Physics.Raycast (transform.position, Front, 3); //checks to see if something is close in front of it
 			bool objectBehind = Physics.Raycast (transform.position, Behind, 1); //checks to see if something is close behind it
-
             
 			blackboard["targetDistance"] = localPos.magnitude;
             blackboard["targetInFront"] = heading.z > 0;
