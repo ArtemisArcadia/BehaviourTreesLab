1cd40ade5674105b8f5bb7387879c621ccf17b1a
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2017-10-26 23:07:51 +0100

    Added raycasting and new behaviours.

--- TankTutorial/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ TankTutorial/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -16,12 +16,14 @@ namespace Complete
 
             switch (m_Behaviour) {
 
-                case 1:
-                    return SpinBehaviour(-0.05f, 1f);
-                case 2:
-                    return TrackBehaviour();
+			case 0:
+				return Fun ();
+            case 1:
+                return SpinBehaviour(-0.05f, 1f);
+            case 2:
+				return Cowardly();
 			case 3:
-				return WildBehaviour();
+				return Wild();
 
                 default:
                     return new Root (new Action(()=> Turn(0.1f)));
@@ -37,6 +39,10 @@ namespace Complete
         private Node RandomFire() {
             return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
         }
+
+		private Node StopMoving(){
+			return new Action (() => Move (0));
+		}
 
 
         /* Example behaviour trees */
@@ -73,7 +79,7 @@ namespace Complete
             );
         }
 
-		private Root WildBehaviour() {
+		private Root Wild() {
 			return new Root (
 				new Service (0.2f, UpdatePerception,
 					new Selector (
@@ -83,6 +89,7 @@ namespace Complete
 							new Sequence(
 								new Action(() => Move(1)),
 								new Action(() => Turn(1)),
+								new Action(() => Turn(-1),
 								new Action(() => Fire(0))
 							)
 						)
@@ -92,14 +99,155 @@ namespace Complete
 			);
 		}
 
+
+
+			
+
+
+		//follows when close and fires, evades
+		private Root Fun()
+		{
+
+
+			return new Root (
+				new Service (0.2f, UpdatePerception,
+					new Selector (
+						new BlackboardCondition("objectBlock", Operator.IS_EQUAL,true,
+							Stops.IMMEDIATE_RESTART,
+							new Sequence(
+								new Action(() => Move(-1)),
+								//new Action(() => Turn(2)),
+								new Action(() => StopTurning()))),
+
+						new BlackboardCondition("objectBehind", Operator.IS_EQUAL,true,
+							Stops.IMMEDIATE_RESTART,
+							new Sequence(
+								new Action(() => Move(1)),
+								new Action(() => Turn(2)),
+								new Action(() => StopTurning()))),
+						
+						new BlackboardCondition ("targetOffCentre",
+							Operator.IS_SMALLER_OR_EQUAL, 0.1f,
+							Stops.IMMEDIATE_RESTART,
+
+							new Sequence (
+								new Action (() => StopTurning ()),
+								new Action (() => Fire (1)))),
+
+						new BlackboardCondition ("targetDistance",
+							Operator.IS_GREATER_OR_EQUAL, 10f,
+							Stops.IMMEDIATE_RESTART,
+
+							
+								new Action (() => Move (1))),
+
+						new BlackboardCondition ("targetDistance",
+							Operator.IS_SMALLER_OR_EQUAL, 5f,
+							Stops.IMMEDIATE_RESTART,
+
+							new Action (() => Move (-1))),
+
+
+						new BlackboardCondition ("targetOnRight",
+							Operator.IS_EQUAL, true,
+							Stops.IMMEDIATE_RESTART,
+
+
+							// Turn right toward target
+							new Action (() => Turn (1))),
+						// Turn left toward target
+						new Action (() => Turn (-1))
+
+
+
+
+					)
+				
+				)
+			);
+		}
+
+		//Runs away
+
+		private Root Cowardly()
+		{
+
+
+			return new Root (
+				new Service (0.2f, UpdatePerception,
+					new Selector (
+						new BlackboardCondition("objectBlock", Operator.IS_EQUAL,true,
+							Stops.IMMEDIATE_RESTART,
+							new Sequence(
+								new Action(() => Move(-1)),
+								new Action(() => Turn(2)),
+								new Action(() => StopTurning()))),
+
+						new BlackboardCondition("objectBehind", Operator.IS_EQUAL,true,
+							Stops.IMMEDIATE_RESTART,
+							new Sequence(
+								new Action(() => Move(1)),
+								new Action(() => Turn(2)),
+								new Action(() => StopTurning()))),
+
+						new BlackboardCondition ("targetOffCentre",
+							Operator.IS_SMALLER_OR_EQUAL, 0.1f,
+							Stops.IMMEDIATE_RESTART,
+
+							new Sequence (
+								new Action (() => StopTurning ()),
+								new Action (() => Fire (1)))),
+
+						new BlackboardCondition ("targetDistance",
+							Operator.IS_GREATER_OR_EQUAL, 30f,
+							Stops.IMMEDIATE_RESTART,
+
+							new Action (() => StopMoving())),
+
+						new BlackboardCondition ("targetDistance",
+							Operator.IS_SMALLER_OR_EQUAL, 30f,
+							Stops.IMMEDIATE_RESTART,
+
+							new Action (() => Move (-1))),
+						
+
+
+						new BlackboardCondition ("targetOnRight",
+							Operator.IS_EQUAL, true,
+							Stops.IMMEDIATE_RESTART,
+
+
+							// Turn left away target
+							new Action (() => Turn (1)))
+						// Turn right
+						// 	new Action (() => Turn (1))
+
+
+
+
+					)
+
+				)
+			);
+		}
+
         private void UpdatePerception() {
             Vector3 targetPos = TargetTransform().position;
             Vector3 localPos = this.transform.InverseTransformPoint(targetPos);
             Vector3 heading = localPos.normalized;
-            blackboard["targetDistance"] = localPos.magnitude;
+			Vector3 Front = transform.TransformDirection (Vector3.forward); //variable that holds Vector3 forward value
+			Vector3 Behind = transform.TransformDirection (Vector3.back); //variable that holds Vector 3 back value
+			bool objectBlocking = Physics.Raycast (transform.position, Front, 1); //checks to see if something is close in front of it
+			bool objectBehind = Physics.Raycast (transform.position, Behind, 1); //checks to see if something is close behind it
+
+            
+			blackboard["targetDistance"] = localPos.magnitude;
             blackboard["targetInFront"] = heading.z > 0;
             blackboard["targetOnRight"] = heading.x > 0;
             blackboard["targetOffCentre"] = Mathf.Abs(heading.x);
+			blackboard ["objectBlock"] = objectBlocking; //if something is behind it, returns true
+			blackboard ["objectBehind"] = objectBehind; //if something is behind it, returns true
+
         }
 
     }
